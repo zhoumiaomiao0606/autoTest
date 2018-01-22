2fa9e488cbf8c81b85d561aef25939448cd7de23
zhouguoliang <zglxx2004@163.com>
2018-01-22 09:48:26 +0800

    集成activity

---
+++ src/main/java/com/yunche/loan/config/common/ActivitiConfig.java
@@ -0,0 +1,31 @@
+package com.yunche.loan.config.common;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * Created by zhouguoliang on 2018/1/20.
+ */
+@Configuration
+public class ActivitiConfig {
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @Autowired
+    DruidDataSource druidDataSource;
+
+    @Bean
+    public SpringProcessEngineConfiguration getProcessEngineConfiguration(){
+        SpringProcessEngineConfiguration config =
+                new SpringProcessEngineConfiguration();
+        config.setDataSource(druidDataSource);
+        config.setTransactionManager(transactionManager);
+        config.setDatabaseType("mysql");
+        config.setDatabaseSchemaUpdate("true");
+        return config;
+    }
+}
--- src/main/java/com/yunche/loan/domain/dataObj/AreaVO.java
+++ src/main/java/com/yunche/loan/domain/dataObj/AreaVO.java
@@ -31,5 +31,60 @@ public class AreaVO {
 
         private Integer level;
 
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getLevel() {
+            return level;
+        }
+
+        public void setLevel(Integer level) {
+            this.level = level;
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public List<City> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(List<City> cityList) {
+        this.cityList = cityList;
     }
 }
