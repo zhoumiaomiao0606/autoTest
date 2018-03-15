956fc2ba378ed9e24aed90425527f277b01eadcb
zhengdu <zhengdu1992@163.com>
2018-03-15 13:43:00 +0800

    金融产品计算公式

---
+++ src/main/java/com/yunche/loan/web/controller/ComputeModeController.java
@@ -0,0 +1,25 @@
+package com.yunche.loan.web.controller;
+
+import com.yunche.loan.config.result.ResultBean;
+import com.yunche.loan.domain.entity.CalcParamDo;
+import com.yunche.loan.domain.param.LoanFinancialPlanParam;
+import com.yunche.loan.service.ComputeModeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/computemode")
+public class ComputeModeController {
+    private static final Logger logger = LoggerFactory.getLogger(ComputeModeController.class);
+    @Autowired
+    private ComputeModeService computeModeService;
+    @RequestMapping(value = "/calc",method = RequestMethod.GET)
+    public ResultBean<CalcParamDo> getFormulaResult(@RequestParam("id")int id, @RequestParam("loanAmt")BigDecimal loanAmt, @RequestParam("exeRate")BigDecimal exeRate, @RequestParam("bankBaseRate")BigDecimal bankBaseRate, @RequestParam("year")int year, @RequestParam("carPrice")BigDecimal carPrice){
+        return  computeModeService.calc(id,loanAmt,exeRate,bankBaseRate,year,carPrice);
+    }
+}
