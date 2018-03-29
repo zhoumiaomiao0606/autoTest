fb0c898b387a0e20cab807016d16d788355566ef
liuzhe <718161567@qq.com>
2018-03-29 15:44:24 +0800

    fix 上门家访入参

--- src/main/java/com/yunche/loan/domain/param/LoanHomeVisitParam.java
+++ src/main/java/com/yunche/loan/domain/param/LoanHomeVisitParam.java
@@ -1,16 +1,47 @@
 package com.yunche.loan.domain.param;
 
-import com.yunche.loan.domain.vo.LoanHomeVisitVO;
+import com.yunche.loan.domain.vo.FileVO;
 import lombok.Data;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author liuzhe
  * @date 2018/3/4
  */
 @Data
-public class LoanHomeVisitParam extends LoanHomeVisitVO {
+public class LoanHomeVisitParam {
     /**
      * 业务单ID
      */
     private Long orderId;
+
+    /**
+     * 上门家访单ID
+     */
+    private Long id;
+    /**
+     * 客户ID
+     */
+    private Long customerId;
+    /**
+     * 上门调查员ID
+     */
+    private Long visitSalesmanId;
+
+    private Date visitDate;
+
+    private String surveyReport;
+
+    private String visitAddress;
+
+    private List<FileVO> files = Collections.EMPTY_LIST;
+
+    private Byte status;
+
+    private Date gmtCreate;
+
+    private Date gmtModify;
 }
