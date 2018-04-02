d880bea3f2bbab5b3b8c329c3f5b0e2845d5ce65
wanggang <userbean@outlook.com>
2018-04-02 16:56:10 +0800

    提交

--- src/main/java/com/yunche/loan/mapper/VehicleInformationDOMapper.java
+++ src/main/java/com/yunche/loan/mapper/VehicleInformationDOMapper.java
@@ -7,13 +7,10 @@ import org.apache.ibatis.annotations.Mapper;
 public interface VehicleInformationDOMapper {
     int deleteByPrimaryKey(Long id);
 
-    int insert(VehicleInformationDO record);
-
     int insertSelective(VehicleInformationDO record);
 
     VehicleInformationDO selectByPrimaryKey(Long id);
 
     int updateByPrimaryKeySelective(VehicleInformationDO record);
 
-    int updateByPrimaryKey(VehicleInformationDO record);
 }
