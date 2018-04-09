f38c30613eff094626ff757c7de551ae95fd10bb
wanggang <userbean@outlook.com>
2018-04-09 21:02:19 +0800

    审核信息接口增加了字段

--- src/main/java/com/yunche/loan/domain/vo/UniversalApprovalInfo.java
+++ src/main/java/com/yunche/loan/domain/vo/UniversalApprovalInfo.java
@@ -7,4 +7,7 @@ public class UniversalApprovalInfo {
 
     private String action;
     private String info;
+    private String credit_result;
+    private String credit_info;
+    private String add_condition;
 }
