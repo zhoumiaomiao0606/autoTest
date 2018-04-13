b85495ba3ba7155c5447646c1e91fab64837cc84
liuzhe <718161567@qq.com>
2018-04-13 15:08:08 +0800

    fix

--- src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
@@ -4,7 +4,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.yunche.loan.config.constant.LoanOrderProcessConst;
 import com.yunche.loan.config.constant.LoanProcessEnum;
 import com.yunche.loan.config.result.ResultBean;
 import com.yunche.loan.config.util.SessionUtils;
@@ -22,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
-import sun.misc.ObjectInputFilter;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -1112,6 +1110,18 @@ public class LoanProcessServiceImpl implements LoanProcessService {
                 break;
             case 3:
                 taskStatusText = "已打回";
+                break;
+            case 4:
+                taskStatusText = "未提交";
+                break;
+            case 5:
+                taskStatusText = "未提交";
+                break;
+            case 6:
+                taskStatusText = "未提交";
+                break;
+            case 7:
+                taskStatusText = "未提交";
                 break;
             // null
             default:
