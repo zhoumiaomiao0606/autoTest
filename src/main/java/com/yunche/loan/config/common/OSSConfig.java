18542f80c181501420cda12d7ec0cd8b1a907af9
zhengdu <zhengdu1992@163.com>
2018-04-24 20:04:06 +0800

    资料审核增加下载方法

---
+++ src/main/java/com/yunche/loan/config/common/OSSConfig.java
@@ -0,0 +1,21 @@
+package com.yunche.loan.config.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+@PropertySource(value = "classpath:oss.properties")
+@Data
+public class OSSConfig {
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String bucketName;
+    private String bucketName_android;
+    private String zipBucketName;
+    private String downLoadBasepath;
+    private String zipDiskName;
+}
