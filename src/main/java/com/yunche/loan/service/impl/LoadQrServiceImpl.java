6026125128bf9e6d8903d9f98bb2da425ace7445
zhengdu <zhengdu1992@163.com>
2018-04-25 20:25:18 +0800

    bug

--- src/main/java/com/yunche/loan/service/impl/LoadQrServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/LoadQrServiceImpl.java
@@ -1,12 +1,12 @@
 package com.yunche.loan.service.impl;
 
-import com.google.common.base.Preconditions;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.yunche.loan.config.cache.AppVersionCache;
+import com.yunche.loan.config.common.OSSConfig;
 import com.yunche.loan.config.constant.QrConst;
 import com.yunche.loan.domain.entity.AppVersionDO;
 import com.yunche.loan.service.LoadQrService;
@@ -19,8 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 import static com.yunche.loan.config.constant.AppVersionConst.TERMINAL_TYPE_ANDROID;
 
@@ -29,23 +27,20 @@ public class LoadQrServiceImpl implements LoadQrService {
     @Autowired
     AppVersionCache appVersionCache;
 
+    @Autowired
+    OSSConfig ossConfig;
+
     @Override
     public void downloadQr(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws WriterException, IOException {
         AppVersionDO latestVersion = appVersionCache.getLatestVersion(TERMINAL_TYPE_ANDROID);
-        ResourceBundle bundle = PropertyResourceBundle.getBundle("oss");
-        String endPoint =  bundle.containsKey("endpoint") == false ? "" : bundle.getString("endpoint");
-        String bucketNameAndroid =  bundle.containsKey("bucketName_android") == false ? "" : bundle.getString("bucketName_android");
-
-//        Preconditions.checkNotNull(latestVersion,"APP版本维护中，请稍后重试");
-//        Preconditions.checkNotNull(endPoint,"APP版本维护中，请稍后重试");
-//        Preconditions.checkNotNull(bucketNameAndroid,"APP版本维护中，请稍后重试");
+        String endPoint = ossConfig.getEndpoint();
+        String bucketNameAndroid =ossConfig.getBucketName_android();
         String downUrl;
         if(latestVersion == null || endPoint.isEmpty()|| bucketNameAndroid.isEmpty()){
             downUrl="APP版本维护中，请稍后重试";
         }else{
             downUrl ="https://"+bucketNameAndroid+"."+endPoint+ File.separator+latestVersion.getDownloadUrl();
         }
-
         String dataHandle = new String(downUrl.getBytes(QrConst.UTF_8), QrConst.UTF_8);
         BitMatrix bitMatrix = new MultiFormatWriter().encode(dataHandle, BarcodeFormat.QR_CODE, QrConst.QR_WIDTH, QrConst.QR_HEIGHT);
         httpServletResponse.reset();//清空输出流
