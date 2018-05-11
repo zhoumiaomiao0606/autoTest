4bae026d89d634bb348f44822447461638e55252
zhengdu <zhengdu1992@163.com>
2018-05-11 09:38:38 +0800

    贷后银行还款相关查询

---
+++ src/main/java/com/yunche/loan/mapper/BankRepayQueryDOMapper.java
@@ -0,0 +1,21 @@
+package com.yunche.loan.mapper;
+
+
+import com.yunche.loan.domain.entity.LoanRepayPlanDO;
+import com.yunche.loan.domain.param.BankRepayParam;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
+@Mapper
+public interface BankRepayQueryDOMapper {
+
+
+    BankRepayParam selectByIdCardOrRepayCard(@Param("idCard") String idCard, @Param("repayCard")String repayCard);
+
+    List<LoanRepayPlanDO> selectRepayPlanListByOrderId(@Param("orderId") Long orderId);
+    List<LoanRepayPlanDO>  selectOverdueRepayPlanList(@Param("orderId") Long orderId,
+                                                      @Param("batchDate")Date batchDate, @Param("overdueTimes") Integer overdueTimes);
+}
