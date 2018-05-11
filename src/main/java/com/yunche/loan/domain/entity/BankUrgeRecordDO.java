cfafbf57175eebdb24dd400b1c57010d721bfaef
wanggang <userbean@outlook.com>
2018-05-11 18:23:11 +0800

    1

--- src/main/java/com/yunche/loan/domain/entity/BankUrgeRecordDO.java
+++ src/main/java/com/yunche/loan/domain/entity/BankUrgeRecordDO.java
@@ -2,16 +2,23 @@ package com.yunche.loan.domain.entity;
 
 import lombok.Data;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
 @Data
 public class BankUrgeRecordDO {
     private Long orderId;
 
     private String operator;
 
-    private String sendee;
+    private Long sendee;
 
     private Byte urgeStatus;
 
     private Long bankRepayImpRecordId;
 
+    private Date sendeeDate;
+
+    private Timestamp gmtCreate;
    }
