5eb070ac2660f5b774631c38f72143cab21c391f
zhengdu <zhengdu1992@163.com>
2018-05-24 09:09:59 +0800

    数据权限控制过滤区域银行

--- src/main/java/com/yunche/loan/mapper/UserGroupRelaBankDOMapper.java
+++ src/main/java/com/yunche/loan/mapper/UserGroupRelaBankDOMapper.java
@@ -2,9 +2,11 @@ package com.yunche.loan.mapper;
 
 import com.yunche.loan.domain.entity.UserGroupRelaBankDO;
 import com.yunche.loan.domain.entity.UserGroupRelaBankDOKey;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface UserGroupRelaBankDOMapper {
     int deleteByPrimaryKey(UserGroupRelaBankDOKey key);
 
