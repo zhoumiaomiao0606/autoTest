46a2ac43eb81b7005b012399f1a48f7d9ad28cc2
zhengdu <zhengdu1992@163.com>
2018-06-27 14:44:14 +0800

    bankinterface : 参数配置

---
+++ src/main/java/com/yunche/loan/config/common/SysConfig.java
@@ -0,0 +1,18 @@
+package com.yunche.loan.config.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+@PropertySource(value = "classpath:sysconfig.properties")
+@Data
+public class SysConfig {
+    private String servierIP;//ftp 服务器IP
+    private String userName;//ftp 用户名
+    private String password;//ftp 密码
+    private String serverpath;//ftp 远程服务器地址
+    private int port;//ftp 端口
+}
