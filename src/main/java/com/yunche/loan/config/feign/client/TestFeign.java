3b3ebc81558fb54bc2ef77aa536f787ea7034af0
wanggang <userbean@outlook.com>
2018-07-06 10:54:53 +0800

    fix

--- src/main/java/com/yunche/loan/config/feign/client/TestFeign.java
+++ src/main/java/com/yunche/loan/config/feign/client/TestFeign.java
@@ -13,10 +13,10 @@ import javax.validation.constraints.NotNull;
 @FeignClient(name = "testFeign" ,url = "http://wwzkdci.hk1.mofasuidao.cn/")
 public interface TestFeign {
     @PostMapping(value = "/api/v1/loanorder/icbc/query", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public boolean query(@Validated @NotNull @RequestBody ICBCApiCallbackParam.Callback callback);
+    public String query(@Validated @NotNull @RequestBody ICBCApiCallbackParam.Callback callback);
 
 
 
     @PostMapping(value = "/api/v1/loanorder/icbc/term", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public boolean term(@Validated @NotNull @RequestBody ICBCApiCallbackParam.Callback callback);
+    public String term(@Validated @NotNull @RequestBody ICBCApiCallbackParam.Callback callback);
 }
