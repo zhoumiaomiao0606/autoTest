d3b50ae8f6af8f361e24575bbe3679e4604dcbfe
wanggang <userbean@outlook.com>
2018-07-17 12:00:18 +0800

    fiz

--- src/main/java/com/yunche/loan/web/controller/ICBCController.java
+++ src/main/java/com/yunche/loan/web/controller/ICBCController.java
@@ -59,7 +59,7 @@ public class ICBCController {
     //请求接口
     @PostMapping (value = "/test", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResultBean<Long> test() {
-        bankSolutionService.creditAutomaticCommit(new Long("1806291133480804371"));
+        bankSolutionService.creditAutomaticCommit(new Long("1807171134134698396"));
         return ResultBean.ofSuccess(null);
     }
 
