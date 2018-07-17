006501bd2b3b5c0b585a209ba9057a4a333b29bf
wanggang <userbean@outlook.com>
2018-07-17 18:31:14 +0800

    fox

--- src/main/java/com/yunche/loan/config/feign/response/CreditCardApplyResponse.java
+++ src/main/java/com/yunche/loan/config/feign/response/CreditCardApplyResponse.java
@@ -5,4 +5,16 @@ import lombok.Data;
 
 @Data
 public class CreditCardApplyResponse extends BasicResponse {
+    private String status;
+    private String notes;
+    private String updatetime;
+    private String cardno;
+    private String custname;
+    private String idno;
+    private String divitype;
+    private String loandate;
+    private String loanbrno;
+    private String accno;
+    private String amount;
+    private String term;
 }
