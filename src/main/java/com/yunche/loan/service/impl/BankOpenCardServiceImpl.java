32aca92225a671a031c610a09438f689529a41d5
zhengdu <zhengdu1992@163.com>
2018-07-25 10:36:51 +0800

    bankinterface : bug

--- src/main/java/com/yunche/loan/service/impl/BankOpenCardServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/BankOpenCardServiceImpl.java
@@ -118,18 +118,18 @@ public class BankOpenCardServiceImpl implements BankOpenCardService{
 
         UniversalCustomerDetailVO universalCustomerDetailVO = loanQueryDOMapper.selectUniversalCustomerDetail(orderId,customerId);
         BankInterfaceSerialVO bankInterfaceSerialVO = new BankInterfaceSerialVO();
-        BankInterfaceSerialDO serialDO = bankInterfaceSerialDOMapper.selectByCustomerIdAndTransCode(customerId, IDict.K_TRANS_CODE.CREDITCARDAPPLY);
-        if(serialDO!=null) {
-            BeanUtils.copyProperties(serialDO, bankInterfaceSerialVO);
-            BankInterfaceFileSerialDO bankInterfaceFileSerialDO = bankInterfaceFileSerialDOMapper.selectByPrimaryKey(Long.valueOf(serialDO.getSerialNo()));
-            if (bankInterfaceFileSerialDO != null) {
-                if (bankInterfaceFileSerialDO.getSuccess().equals(IDict.K_YORN.K_YORN_NO) && bankInterfaceFileSerialDO.getError().equals((byte) 2)) {
-                    bankInterfaceSerialVO.setMergeStatus(String.valueOf(IDict.K_YORN.K_YORN_NO));
-                } else {
-                    bankInterfaceSerialVO.setMergeStatus(String.valueOf(IDict.K_YORN.K_YORN_YES));
-                }
-            }
-        }
+//        BankInterfaceSerialDO serialDO = bankInterfaceSerialDOMapper.selectByCustomerIdAndTransCode(customerId, IDict.K_TRANS_CODE.CREDITCARDAPPLY);
+//        if(serialDO!=null) {
+//            BeanUtils.copyProperties(serialDO, bankInterfaceSerialVO);
+//            BankInterfaceFileSerialDO bankInterfaceFileSerialDO = bankInterfaceFileSerialDOMapper.selectByPrimaryKey(Long.valueOf(serialDO.getSerialNo()));
+//            if (bankInterfaceFileSerialDO != null) {
+//                if (bankInterfaceFileSerialDO.getSuccess().equals(IDict.K_YORN.K_YORN_NO) && bankInterfaceFileSerialDO.getError().equals((byte) 2)) {
+//                    bankInterfaceSerialVO.setMergeStatus(String.valueOf(IDict.K_YORN.K_YORN_NO));
+//                } else {
+//                    bankInterfaceSerialVO.setMergeStatus(String.valueOf(IDict.K_YORN.K_YORN_YES));
+//                }
+//            }
+//        }
         LoanProcessDO loanProcessDO = loanProcessDOMapper.selectByPrimaryKey(orderId);
         Preconditions.checkNotNull(loanProcessDO,"流程不存在");
         Byte telephoneVerify = loanProcessDO.getTelephoneVerify();
