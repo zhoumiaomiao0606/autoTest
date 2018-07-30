a2abe1766f424a6c0bb4250e812f025a414cd267
zhengdu <zhengdu1992@163.com>
2018-07-30 10:27:44 +0800

    v_1.1.6:催保分配工作台

--- src/main/java/com/yunche/loan/config/feign/client/ICBCFeignNormal.java
+++ src/main/java/com/yunche/loan/config/feign/client/ICBCFeignNormal.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "iCBCFeignNormal" ,url = "http://localhost:18090/")
+@FeignClient(name = "iCBCFeignNormal" ,url = "http://122.225.203.102:18090/")
 public interface ICBCFeignNormal {
 
 
-    @RequestMapping(value = "/api/v1/test/icbc/apply/filedownload",method = RequestMethod.GET)
+    @RequestMapping(value = "/api/v1/icbc/apply/filedownload",method = RequestMethod.GET)
     public boolean filedownload(@RequestParam(value = "filesrc") String filesrc);
 
 
