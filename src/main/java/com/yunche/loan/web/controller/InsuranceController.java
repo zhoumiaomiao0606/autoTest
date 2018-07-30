2c103710d9c95860bba92f73057823d53ab49bbf
liuzhe <718161567@qq.com>
2018-07-30 17:07:20 +0800

    fix

--- src/main/java/com/yunche/loan/web/controller/InsuranceController.java
+++ src/main/java/com/yunche/loan/web/controller/InsuranceController.java
@@ -7,6 +7,7 @@ import com.yunche.loan.domain.query.RiskQuery;
 import com.yunche.loan.domain.vo.RiskQueryVO;
 import com.yunche.loan.service.InsuranceService;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -42,43 +43,48 @@ public class InsuranceController {
      * 录入车辆保险
      */
     @PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResultBean update(@RequestBody  InsuranceUpdateParam param) {
+    public ResultBean update(@RequestBody InsuranceUpdateParam param) {
         insuranceService.update(param);
         return ResultBean.ofSuccess(null, "保存成功");
     }
 
     /**
      * 出险列表
+     *
      * @param jjq
      * @return
      */
     @PostMapping(value = "/risklist", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResultBean<List<RiskQueryVO>> riskList(@RequestBody RiskQuery query){
+    public ResultBean<List<RiskQueryVO>> riskList(@RequestBody RiskQuery query) {
         return insuranceService.riskList(query);
     }
+
     /*
     * 出险详情
      */
     @GetMapping(value = "/riskdetail")
-    public ResultBean riskDeatil(@RequestParam String order_id,@RequestParam String insurance_year){
-        return ResultBean.ofSuccess(insuranceService.riskDetail(Long.valueOf(order_id),Byte.valueOf(insurance_year)));
+    public ResultBean riskDeatil(@RequestParam String order_id, @RequestParam String insurance_year) {
+        return ResultBean.ofSuccess(insuranceService.riskDetail(Long.valueOf(order_id), Byte.valueOf(insurance_year)));
     }
+
     /**
      * 出险插入
      */
     @PostMapping(value = "riskinsert", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResultBean riskInsert(@RequestBody @Validated InsuranceRisksParam param){
+    public ResultBean riskInsert(@RequestBody @Validated InsuranceRisksParam param) {
         insuranceService.riskInsert(param);
         return ResultBean.ofSuccess(null, "保存成功");
     }
+
     /**
      * 出险更新
      */
     @PostMapping(value = "riskupdate", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResultBean riskUpdate(@RequestBody @Validated InsuranceRisksParam param){
+    public ResultBean riskUpdate(@RequestBody @Validated InsuranceRisksParam param) {
         insuranceService.riskUpdate(param);
         return ResultBean.ofSuccess(null, "更新成功");
     }
+
     /**
      * 出险删除
      */
