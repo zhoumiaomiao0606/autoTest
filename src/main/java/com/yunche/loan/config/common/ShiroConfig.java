f52d3268556960aba12ee717f9858c9eb3e3cee3
wanggang <userbean@outlook.com>
2018-08-01 12:57:35 +0800

    fix

--- src/main/java/com/yunche/loan/config/common/ShiroConfig.java
+++ src/main/java/com/yunche/loan/config/common/ShiroConfig.java
@@ -89,7 +89,7 @@ public class ShiroConfig {
             filterChainDefinitionMap.put("/api/v1/loadqr/query", "anon");
             filterChainDefinitionMap.put("/api/v1/loanorder/icbc/creditresult", "anon");
             filterChainDefinitionMap.put("/api/v1/loanorder/icbc/creditreturn", "anon");
-            filterChainDefinitionMap.put("/api/v1/loanorder/icbc/multimediaUploadreturn", "anon");
+            filterChainDefinitionMap.put("/api/v1/loanorder/icbc/multimediareturn", "anon");
             filterChainDefinitionMap.put("/api/v1/loanorder/icbc/creditcardresult", "anon");
             filterChainDefinitionMap.put("/api/v1/loanorder/icbc/fileNotice", "anon");
             filterChainDefinitionMap.put("/api/v1/loanorder/icbc/artificialgainImage", "anon");
