6449b0d7ec49f9b7e2c1b2445517d6406d97d792
zmx111222 <583976359@qq.com>
2018-08-10 15:45:17 +0800

    导出银行征信--社会征信---增加申请时间过滤 =_=

--- src/main/java/com/yunche/loan/domain/param/ExportSocialCreditQueryVerifyParam.java
+++ src/main/java/com/yunche/loan/domain/param/ExportSocialCreditQueryVerifyParam.java
@@ -11,6 +11,10 @@ public class ExportSocialCreditQueryVerifyParam
     private  String startDate;
     private  String endDate;
 
+    //征信申请时间
+    private String startCreditGmtCreate;
+    private String endCreditGmtCreate;
+
     //合伙人团队列表
     private List<Long> partnerList;
     //贷款银行列表
