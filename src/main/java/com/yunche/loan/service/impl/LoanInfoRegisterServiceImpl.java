324f78f9629a150a8fea0b4fa5af3cb2e6eeb912
liuzhe <718161567@qq.com>
2018-08-11 00:58:37 +0800

    fix

--- src/main/java/com/yunche/loan/service/impl/LoanInfoRegisterServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/LoanInfoRegisterServiceImpl.java
@@ -14,13 +14,16 @@ import com.yunche.loan.service.LoanInfoRegisterService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
-public class LoanInfoRegisterServiceImpl  implements LoanInfoRegisterService{
+public class LoanInfoRegisterServiceImpl implements LoanInfoRegisterService {
 
     @Autowired
     private LoanQueryDOMapper loanQueryDOMapper;
-    
+
     @Autowired
     private LoanOrderDOMapper loanOrderDOMapper;
 
@@ -40,10 +43,9 @@ public class LoanInfoRegisterServiceImpl  implements LoanInfoRegisterService{
     private LoanFinancialPlanService loanFinancialPlanService;
 
 
-
     @Override
     public ResultBean detail(Long orderId) {
-        Preconditions.checkNotNull(orderId,"参数有误");
+        Preconditions.checkNotNull(orderId, "参数有误");
 
         RecombinationVO recombinationVO = new RecombinationVO<>();
         //客户基本信息
@@ -63,80 +65,112 @@ public class LoanInfoRegisterServiceImpl  implements LoanInfoRegisterService{
 
     /**
      * 贷款信息注册 更新
+     *
      * @param loanInfoRegisterParam
      * @return
      */
     @Override
+    @Transactional
     public ResultBean update(LoanInfoRegisterParam loanInfoRegisterParam) {
-        Preconditions.checkNotNull(loanInfoRegisterParam,"参数有误");
-        Preconditions.checkNotNull(loanInfoRegisterParam.getOrderId(),"业务单号未传");
+        Preconditions.checkNotNull(loanInfoRegisterParam, "参数有误");
+        Preconditions.checkNotNull(loanInfoRegisterParam.getOrderId(), "业务单号未传");
+
         LoanOrderDO loanOrderDO = loanOrderDOMapper.selectByPrimaryKey(loanInfoRegisterParam.getOrderId());
-        Preconditions.checkNotNull(loanOrderDO,"订单信息不存在");
+        Preconditions.checkNotNull(loanOrderDO, "订单信息不存在");
 
 
+        // loanCarInfo
         Long loanCarInfoId = loanOrderDO.getLoanCarInfoId();
 
-
         LoanCarInfoDO carInfoDO = new LoanCarInfoDO();
-
         carInfoDO.setVehicleProperty(loanInfoRegisterParam.getVehicleProperty());
         carInfoDO.setCarType(loanInfoRegisterParam.getCarType());
         carInfoDO.setCarDetailId(loanInfoRegisterParam.getCarDetail().getId());
         carInfoDO.setCarDetailName(loanInfoRegisterParam.getCarDetail().getName());
 
+        if (null != loanCarInfoId) {
 
-        if(loanCarInfoId !=null){
+            // update
             carInfoDO.setId(loanCarInfoId);
+            carInfoDO.setGmtModify(new Date());
             int count = loanCarInfoDOMapper.updateByPrimaryKeySelective(carInfoDO);
-            Preconditions.checkArgument(count>0,"更新车辆信息失败");
-        }else{
+            Preconditions.checkArgument(count > 0, "保存车辆信息失败");
 
+        } else {
+
+            // insert
             Long loanBaseInfoId = loanOrderDO.getLoanBaseInfoId();
             LoanBaseInfoDO loanBaseInfoDO = loanBaseInfoDOMapper.selectByPrimaryKey(loanBaseInfoId);
             Long partnerId = loanBaseInfoDO.getPartnerId();
             carInfoDO.setPartnerId(partnerId);
+            carInfoDO.setGmtCreate(new Date());
+            carInfoDO.setGmtModify(new Date());
+
             int count = loanCarInfoDOMapper.insertSelective(carInfoDO);
-            Preconditions.checkArgument(count>0,"插入信息失败");
-            Long carinfoid = carInfoDO.getId();
-            loanOrderDO.setLoanCarInfoId(carinfoid);
-            int i = loanOrderDOMapper.updateByPrimaryKeySelective(loanOrderDO);
-            Preconditions.checkArgument(i>0,"更新业务申请数据失败");
+            Preconditions.checkArgument(count > 0, "保存车辆信息失败");
+
+            loanOrderDO.setLoanCarInfoId(carInfoDO.getId());
+            loanOrderDO.setGmtModify(new Date());
+            int count2 = loanOrderDOMapper.updateByPrimaryKeySelective(loanOrderDO);
+            Preconditions.checkArgument(count2 > 0, "保存车辆信息失败");
         }
 
+
+        // vehicleInformation
         Long vehicleInformationId = loanOrderDO.getVehicleInformationId();
 
         VehicleInformationDO vehicleInformationDO = new VehicleInformationDO();
         vehicleInformationDO.setId(vehicleInformationId);
         vehicleInformationDO.setColor(loanInfoRegisterParam.getColor());
-        if(vehicleInformationId!=null){
+
+        if (null != vehicleInformationId) {
+
+            // update
             vehicleInformationDO.setId(vehicleInformationId);
             int count = vehicleInformationDOMapper.updateByPrimaryKeySelective(vehicleInformationDO);
-            Preconditions.checkArgument(count>0,"提车资料信息更新失败");
-        }else{
+            Preconditions.checkArgument(count > 0, "保存提车资料失败");
+
+        } else {
+
+            // insert
             int count1 = vehicleInformationDOMapper.insertSelective(vehicleInformationDO);
-            Preconditions.checkArgument(count1>0,"提车资料信息更新失败");
-            Long id = vehicleInformationDO.getId();
-            loanOrderDO.setVehicleInformationId(id);
+            Preconditions.checkArgument(count1 > 0, "保存提车资料失败");
+
+            loanOrderDO.setVehicleInformationId(vehicleInformationDO.getId());
+            loanOrderDO.setGmtModify(new Date());
             int count2 = loanOrderDOMapper.updateByPrimaryKeySelective(loanOrderDO);
-            Preconditions.checkArgument(count2>0,"提车资料信息更新失败");
+            Preconditions.checkArgument(count2 > 0, "保存提车资料失败");
         }
 
+
+        // loanFinancialPlan
         Long loanFinancialPlanId = loanOrderDO.getLoanFinancialPlanId();
 
         LoanFinancialPlanDO loanFinancialPlanDO = new LoanFinancialPlanDO();
         BeanUtils.copyProperties(loanInfoRegisterParam.getLoanFinancialPlanParam(), loanFinancialPlanDO);
-        if(loanFinancialPlanId!=null){
+
+        if (null != loanFinancialPlanId) {
+
+            // update
             loanFinancialPlanDO.setId(loanFinancialPlanId);
+            loanFinancialPlanDO.setGmtModify(new Date());
             int count = loanFinancialPlanDOMapper.updateByPrimaryKeySelective(loanFinancialPlanDO);
-            Preconditions.checkArgument(count>0,"插入金融方案计划失败");
-        }else{
+            Preconditions.checkArgument(count > 0, "插入金融方案计划失败");
+
+        } else {
+
+            // insert
+            loanFinancialPlanDO.setGmtCreate(new Date());
+            loanFinancialPlanDO.setGmtModify(new Date());
             int count = loanFinancialPlanDOMapper.insertSelective(loanFinancialPlanDO);
-            Preconditions.checkArgument(count>0,"插入金融方案计划失败");
-            Long id = loanFinancialPlanDO.getId();
-            loanOrderDO.setLoanFinancialPlanId(id);
-            int lines =loanOrderDOMapper.updateByPrimaryKeySelective(loanOrderDO);
-            Preconditions.checkArgument(count>0,"插入金融方案计划失败");
+            Preconditions.checkArgument(count > 0, "插入金融方案计划失败");
+
+            loanOrderDO.setLoanFinancialPlanId(loanFinancialPlanDO.getId());
+            loanOrderDO.setGmtModify(new Date());
+            int count2 = loanOrderDOMapper.updateByPrimaryKeySelective(loanOrderDO);
+            Preconditions.checkArgument(count2 > 0, "插入金融方案计划失败");
         }
-        return ResultBean.ofSuccess(null,"保存成功");
+
+        return ResultBean.ofSuccess(null, "保存成功");
     }
 }
