175a90bdfec38c3b801331172caeceb1b8cf6b54
wanggang <userbean@outlook.com>
2018-08-16 15:49:13 +0800

    fix

---
+++ src/main/java/com/yunche/loan/web/controller/LegworkReimbursementController.java
@@ -0,0 +1,14 @@
+package com.yunche.loan.web.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/loanorder/legworkreimbursement")
+public class LegworkReimbursementController {
+
+
+}
