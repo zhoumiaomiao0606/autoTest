e592bc37586ef2466e8338919863b61311d0327d
wanggang <userbean@outlook.com>
2018-08-17 15:59:03 +0800

    日志

---
+++ src/main/java/com/yunche/loan/domain/vo/LegworkReimbursementVO.java
@@ -0,0 +1,18 @@
+package com.yunche.loan.domain.vo;
+
+import lombok.Data;
+
+@Data
+public class LegworkReimbursementVO {
+    private String id;
+    private String transFee;
+    private String hotelFee;
+    private String eatFee;
+    private String busiFee;
+    private String otherFee;
+    private String gmtUser;
+    private String gmtUserName;
+    private String gmtCreate;
+    private String gmtUpdateTime;
+    private String status;
+}
