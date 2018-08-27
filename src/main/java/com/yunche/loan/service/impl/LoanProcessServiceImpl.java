a3dd033891603ee7c8dec13a2c40fcdfa90191aa
admin <123456>
2018-08-27 09:15:13 +0800

    征信提交大数据校验

--- src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
@@ -1282,7 +1282,7 @@ public class LoanProcessServiceImpl implements LoanProcessService {
         // 【征信申请】时，若身份证有效期<=（today+7），不允许提交，提示“身份证已过期，不允许申请贷款”
         if (CREDIT_APPLY.getCode().equals(taskDefinitionKey) && ACTION_PASS.equals(action)) {
             // 众安征信接口校验（先关闭等ios过审核）
-            /*List<LoanCustomerDO> loanCustomerDOS = loanCustomerDOMapper.selectCusByOrderId(loanOrderDO.getId());
+            List<LoanCustomerDO> loanCustomerDOS = loanCustomerDOMapper.selectCusByOrderId(loanOrderDO.getId());
             for (LoanCustomerDO loanCustomerDO : loanCustomerDOS) {
                 ZhonganInfoDO zhonganInfoDO = zhonganInfoDOMapper.selectZNByOrderIdAndIdcard(loanOrderDO.getId(), loanCustomerDO.getIdCard());
                 if (zhonganInfoDO == null) {
@@ -1292,7 +1292,7 @@ public class LoanProcessServiceImpl implements LoanProcessService {
                         throw new BizException("客户:" + zhonganInfoDO.getCustomerName() + zhonganInfoDO.getResultMessage() + ",无法提交征信");
                     }
                 }
-            }*/
+            }
 
             // 客户信息校验
             LoanCustomerDO loanCustomerDO = getLoanCustomer(loanOrderDO.getLoanCustomerId());
