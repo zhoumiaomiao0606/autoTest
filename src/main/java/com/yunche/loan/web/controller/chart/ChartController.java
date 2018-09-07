10f798792ff017e03fe11dbb3b8b1b797498974b
zmx111222 <583976359@qq.com>
2018-09-07 16:57:09 +0800

    报表

--- src/main/java/com/yunche/loan/web/controller/chart/ChartController.java
+++ src/main/java/com/yunche/loan/web/controller/chart/ChartController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
  **/
 @CrossOrigin
 @RestController
-@RequestMapping("/api/v1/app/loanorder/chart")
+@RequestMapping("/api/v1/loanorder/chart")
 public class ChartController
 {
     @Autowired
