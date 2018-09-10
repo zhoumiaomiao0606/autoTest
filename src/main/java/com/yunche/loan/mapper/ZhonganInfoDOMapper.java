1dd7103ec8502873c05b71beecc4486a558b137d
admin <123456>
2018-09-10 11:08:25 +0800

    报表

--- src/main/java/com/yunche/loan/mapper/ZhonganInfoDOMapper.java
+++ src/main/java/com/yunche/loan/mapper/ZhonganInfoDOMapper.java
@@ -2,7 +2,9 @@ package com.yunche.loan.mapper;
 
 import com.yunche.loan.domain.entity.LoanCustomerDO;
 import com.yunche.loan.domain.entity.ZhonganInfoDO;
-import com.yunche.loan.domain.vo.ZhonganNameVO;
+import com.yunche.loan.domain.query.BaseQuery;
+import com.yunche.loan.domain.query.ContractSetQuery;
+import com.yunche.loan.domain.vo.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -30,4 +32,16 @@ public interface ZhonganInfoDOMapper {
     ZhonganInfoDO selectZNByOrderIdAndIdcard(@Param("orderId")Long orderId,@Param("idCard")String idCard);
 
     ZhonganNameVO selectZhonganName(@Param("orderId")Long orderId);
+
+    List<BusinessApprovalReportVO> businessApproval(BaseQuery query);
+
+    BusinessApprovalReportTotalVO businessApprovalTotal(BaseQuery query);
+
+    List<BusinessApprovalReportVO> businessApprovalExport(BaseQuery query);
+
+    List<ContractSetReportVO> contractSet(ContractSetQuery query);
+
+    ContractSetReportTotalVO contractSetTotal(ContractSetQuery query);
+
+    List<ContractSetReportVO> contractSetExport(ContractSetQuery query);
 }
