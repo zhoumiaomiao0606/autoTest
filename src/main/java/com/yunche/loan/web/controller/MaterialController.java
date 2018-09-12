33b1bb6dc62fb5585e3470d49e421fa7d1c2242e
admin <123456>
2018-09-12 17:06:53 +0800

    logger修正

--- src/main/java/com/yunche/loan/web/controller/MaterialController.java
+++ src/main/java/com/yunche/loan/web/controller/MaterialController.java
@@ -72,12 +72,26 @@ public class MaterialController {
      */
     @GetMapping(value = "/down2oss")
     public ResultBean<String> down2OSS(@RequestParam Long orderId) {
-        return materialService.downloadFiles2OSS(orderId, false);
+        return materialService.downloadFiles2OSS(orderId, true);
     }
 
     @GetMapping(value = "/zipCheck")
     public ResultBean checkZipStatus(@RequestParam(value = "orderId") Long orderId) {
         return materialService.zipCheck(orderId);
+    }
+
+    @GetMapping(value ="/selectallsupfile")
+    public ResultBean selectAllSupFileByOrderId(@RequestParam(value = "orderId") Long orderId) {
+        return ResultBean.ofSuccess(materialService.selectAllSupFileByOrderId(orderId));
+    }
+
+    @GetMapping(value = "/downsup2oss")
+    public ResultBean<String> downSup2OSS(@RequestParam(value = "orderId") Long orderId,@RequestParam(value = "infoSupplementId")Long infoSupplementId) {
+        return materialService.downSupplementFiles2OSS(orderId, false,infoSupplementId);
+    }
+    @GetMapping(value = "/zipsupCheck")
+    public ResultBean checkSupZipStatus(@RequestParam(value = "infoSupplementId") Long infoSupplementId) {
+        return materialService.zipSupCheck(infoSupplementId);
     }
 
 }
