81798032fcdb09ec7d23151eec70808afb9d44a8
admin <123456>
2018-09-19 15:22:35 +0800

    报表改进

--- src/main/java/com/yunche/loan/domain/query/ContractSetQuery.java
+++ src/main/java/com/yunche/loan/domain/query/ContractSetQuery.java
@@ -2,6 +2,8 @@ package com.yunche.loan.domain.query;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ContractSetQuery extends BaseQuery{
     private String bank;
@@ -11,4 +13,9 @@ public class ContractSetQuery extends BaseQuery{
     private String gmtCreateStart1;
 
     private String gmtCreateEnd1;
+
+    //合伙人团队列表
+    private List<Long> partnerList;
+    //贷款银行列表
+    private List<String> bankList;
 }
