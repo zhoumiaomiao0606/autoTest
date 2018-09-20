1382031334c7ccf1fd0f8392124bd6db906f5017
liuzhe <718161567@qq.com>
2018-09-20 11:12:21 +0800

    面签配置-鉴权

--- src/main/java/com/yunche/loan/mapper/ConfVideoFaceTimeDOMapper.java
+++ src/main/java/com/yunche/loan/mapper/ConfVideoFaceTimeDOMapper.java
@@ -2,6 +2,7 @@ package com.yunche.loan.mapper;
 
 import com.yunche.loan.domain.entity.ConfVideoFaceTimeDO;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -19,8 +20,13 @@ public interface ConfVideoFaceTimeDOMapper {
 
     int updateByPrimaryKey(ConfVideoFaceTimeDO record);
 
+    @Delete("DELETE FROM `conf_video_face_time` WHERE `bank_id` = #{bankId}")
+    void deleteAllByBankId(@Param("bankId") Long bankId);
+
     @Delete("DELETE FROM `conf_video_face_time`")
     void deleteAll();
+
+    List<ConfVideoFaceTimeDO> listByBankId(@Param("bankId") Long bankId);
 
     List<ConfVideoFaceTimeDO> listAll();
 }
