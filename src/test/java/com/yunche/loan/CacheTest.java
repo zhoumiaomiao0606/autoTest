443f68cae7e21153c3a5e5ebdb70bd9bff2b6299
admin <123456>
2018-09-27 14:44:10 +0800

    Merge branch 'v_1.1.12' of /Users/admin/IdeaProjects/111/loanBusiness with conflicts.

--- src/test/java/com/yunche/loan/CacheTest.java
+++ src/test/java/com/yunche/loan/CacheTest.java
@@ -3,6 +3,7 @@ package com.yunche.loan;
 import com.google.common.collect.Lists;
 import com.yunche.loan.config.cache.BankCache;
 import com.yunche.loan.config.cache.TokenCache;
+import com.yunche.loan.config.thread.ThreadPool;
 import com.yunche.loan.config.util.CarLoanHttpUtil;
 import com.yunche.loan.config.util.OpenApiUtil;
 import com.yunche.loan.config.util.ZhongAnHttpUtil;
@@ -14,17 +15,21 @@ import com.yunche.loan.mapper.PartnerDOMapper;
 import com.yunche.loan.service.AppLoanOrderService;
 import com.yunche.loan.service.AuxiliaryService;
 import com.yunche.loan.service.MaterialService;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class CacheTest extends BaseTest {
+public class CacheTest  {
     @Autowired
     private TokenCache tokenCache;
     @Autowired
@@ -41,6 +46,106 @@ public class CacheTest extends BaseTest {
     @Resource
     MaterialService materialService;
 
+    @Test
+    public void DFDSf(){
+        List<String> list =new ArrayList<>();
+        list.add(null);
+        list.add("1");
+        list.add("1");
+        HashSet h =new HashSet(list);
+        list.clear();
+        list.addAll(h);
+        System.out.println(list.size());
+        list.retainAll(Collections.singleton(null));
+        System.out.println(list.size());
+
+        List<String> list1 =new ArrayList<>();
+        list1.add("2");
+        list.retainAll(list1);
+        System.out.println(list.size());
+    }
+   // @Test
+    public void DFDS(){
+        List list =new ArrayList(2);
+        list.add(1);
+        list.add(2);
+        list.remove(0);
+        System.out.println(list.get(0));
+    }
+    //@Test
+    public void ADdasd(){
+        List list =new ArrayList(10);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        for(int i =0;i<list.size();i++) {
+            ThreadPool.executorService.execute(() -> {
+
+                synchronized (list) {
+                    System.out.println(Thread.currentThread().getId() + "|" + list.get(0));
+                    list.remove(0);
+                }
+            });
+        }
+    }
+
+    //@Test
+    public void doDo(){
+        String fileName = "TelUser10.xlsx";
+        //创建workbook
+        File file = new File( fileName);
+        FileOutputStream out = null;
+        XSSFWorkbook workbook = null;
+        try {
+            out = new FileOutputStream(file);
+            workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet();
+            sheet.addMergedRegion(new CellRangeAddress(0,1,0,0));
+            sheet.addMergedRegion(new CellRangeAddress(0,1,1,1));
+            sheet.addMergedRegion(new CellRangeAddress(0,0,2,4));
+            sheet.addMergedRegion(new CellRangeAddress(0,0,5,7));
+            sheet.addMergedRegion(new CellRangeAddress(0,0,8,10));
+            sheet.addMergedRegion(new CellRangeAddress(0,0,11,13));
+            sheet.addMergedRegion(new CellRangeAddress(0,0,14,16));
+            XSSFRow headRow = sheet.createRow(0);
+            XSSFCell cell0 = headRow.createCell(0);
+            cell0.setCellValue("大区");
+            XSSFCell cell1= headRow.createCell(1);
+            cell1.setCellValue("合伙人");
+            XSSFCell cell2 = headRow.createCell(2);
+            cell2.setCellValue("电审结果-打回");
+            XSSFCell cell3 = headRow.createCell(5);
+            cell3.setCellValue("电审结果-资料增补");
+            XSSFCell cell4 = headRow.createCell(8);
+            cell4.setCellValue("电审结果-通过");
+            XSSFCell cell5 = headRow.createCell(11);
+            cell5.setCellValue("电审结果-弃单");
+            XSSFCell cell6 = headRow.createCell(14);
+            cell6.setCellValue("经办汇总");
+
+            XSSFRow headRow1 = sheet.createRow(1);
+            ArrayList<String> header1 = Lists.newArrayList("单量", "占比","贷款金额",
+                    "单量", "占比","贷款金额","单量", "占比","贷款金额","单量", "占比","贷款金额","单量", "占比","贷款金额"
+            );
+            for (int i = 0; i < header1.size(); i++) {
+                XSSFCell cell = headRow1.createCell(i+1);
+                cell.setCellValue(header1.get(i));
+            }
+
+
+            workbook.write(out);
+        }catch (Exception e){
+
+        }
+    }
+
+
     // @Test
 
     public void getAAA() {
@@ -53,7 +158,7 @@ public class CacheTest extends BaseTest {
         System.out.println("tmp/123".substring(4));
     }
 
-    @Test
+    //@Test
     public void doY() {
         materialService.downSupplementFiles2OSS(Long.valueOf("1809051406599576357"), true, Long.valueOf("193"));
     }
