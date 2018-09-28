77f2280336e6634fd6829b6b32c4c6e7e95e622f
admin <123456>
2018-09-28 10:54:11 +0800

    时间改进

---
+++ src/main/java/com/yunche/loan/domain/entity/CurrentNodeManagerDO.java
@@ -0,0 +1,93 @@
+package com.yunche.loan.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class CurrentNodeManagerDO {
+    private Long orderId;
+
+    private String usertaskCreditApplyCreateTime;
+
+    private String usertaskBankCreditRecordCreateTime;
+
+    private String usertaskSocialCreditRecordCreateTime;
+
+    private String usertaskLoanApplyCreateTime;
+
+    private String usertaskVisitVerifyCreateTime;
+
+    private String usertaskTelephoneVerifyCreateTime;
+
+    private String usertaskVehicleInformationCreateTime;
+
+    private String usertaskMaterialReviewCreateTime;
+
+    private String usertaskMaterialPrintReviewCreateTime;
+
+    private String usertaskInstallGpsCreateTime;
+
+    private String usertaskApplyLicensePlateDepositInfoCreateTime;
+
+    private String usertaskCarInsuranceCreateTime;
+
+    private String usertaskBusinessPayCreateTime;
+
+    private String usertaskBusinessReviewCreateTime;
+
+    private String usertaskLoanReviewCreateTime;
+
+    private String usertaskRemitReviewCreateTime;
+
+    private String usertaskMaterialManageCreateTime;
+
+    private String usertaskBankLendRecordCreateTime;
+
+    private String usertaskBankCardSendCreateTime;
+
+    private String usertaskRefundApplyCreateTime;
+
+    private String usertaskFinancialSchemeModifyApplyCreateTime;
+
+    private String usertaskCreditApplyGmtUserName;
+
+    private String usertaskBankCreditRecordGmtUserName;
+
+    private String usertaskSocialCreditRecordGmtUserName;
+
+    private String usertaskLoanApplyGmtUserName;
+
+    private String usertaskVisitVerifyGmtUserName;
+
+    private String usertaskTelephoneVerifyGmtUserName;
+
+    private String usertaskVehicleInformationGmtUserName;
+
+    private String usertaskMaterialReviewGmtUserName;
+
+    private String usertaskMaterialPrintReviewGmtUserName;
+
+    private String usertaskInstallGpsGmtUserName;
+
+    private String usertaskApplyLicensePlateDepositInfoGmtUserName;
+
+    private String usertaskCarInsuranceGmtUserName;
+
+    private String usertaskBusinessPayGmtUserName;
+
+    private String usertaskBusinessReviewGmtUserName;
+
+    private String usertaskLoanReviewGmtUserName;
+
+    private String usertaskRemitReviewGmtUserName;
+
+    private String usertaskMaterialManageGmtUserName;
+
+    private String usertaskBankLendRecordGmtUserName;
+
+    private String usertaskBankCardSendGmtUserName;
+
+    private String usertaskRefundApplyGmtUserName;
+
+    private String usertaskFinancialSchemeModifyApplyGmtUserName;
+    
+}
