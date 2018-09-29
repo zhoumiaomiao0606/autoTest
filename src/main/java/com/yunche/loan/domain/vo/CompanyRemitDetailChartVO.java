c66df6078d3abdc213cc50002c09936a92c1fb9e
admin <123456>
2018-09-29 14:52:30 +0800

    报表改进

--- src/main/java/com/yunche/loan/domain/vo/CompanyRemitDetailChartVO.java
+++ src/main/java/com/yunche/loan/domain/vo/CompanyRemitDetailChartVO.java
@@ -21,13 +21,6 @@ public class CompanyRemitDetailChartVO
     //身份证号
     private String customer_id_card;
 
-
-    //贷款银行
-    private String bank;
-
-    //业务团队
-    private String partner_name;
-
     //业务员
     private String salesman_name;
 
@@ -37,24 +30,29 @@ public class CompanyRemitDetailChartVO
     //省份---上牌地
     private String apply_license_plate_area;
 
-    //车型
-    private String car_type;
+    //业务团队
+    private String partner_name;
 
+    //贷款银行
+    private String bank;
 
     //执行利率
     private String sign_rate;
 
+    //车型
+    private String car_type;
+
     //首付款
     private String down_payment_money;
-
-    //贷款金额
-    private BigDecimal loan_amount;
 
     //银行分期本金
     private BigDecimal bank_period_principal;
 
     //打款金额
     private BigDecimal remit_amount;
+
+    //贷款金额
+    private BigDecimal loan_amount;
 
     //提交时间
     private Date submitTime;
