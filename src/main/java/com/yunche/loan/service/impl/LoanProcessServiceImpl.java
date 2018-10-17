4f05fef20995bd2027fcc430d6100404898d9a5e
admin <123456>
2018-10-17 11:42:19 +0800

    贷款申请配置画

--- src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/LoanProcessServiceImpl.java
@@ -3301,7 +3301,7 @@ public class LoanProcessServiceImpl implements LoanProcessService {
                     compardNum(confLoanApplyDO.getFinancial_service_fee_compard(), financialServiceFee.divide(new BigDecimal("10000")), confLoanApplyDO.getFinancial_service_fee(), "金融手续费1");
                 }
                 if (confLoanApplyDO.getCar_ratio() != null && confLoanApplyDO.getCar_ratio_compard() != null) {
-                    compardNum(confLoanApplyDO.getCar_ratio_compard(), financialServiceFee, loanFinancialPlanDO.getCarPrice().multiply(confLoanApplyDO.getCar_ratio()), "金融手续费2");
+                    compardNum(confLoanApplyDO.getCar_ratio_compard(), financialServiceFee, loanFinancialPlanDO.getCarPrice().multiply(confLoanApplyDO.getCar_ratio().divide(new BigDecimal("100"))), "金融手续费2");
                 }
                 if (confLoanApplyDO.getLoan_ratio() != null && confLoanApplyDO.getLoan_ratio_compare() != null) {
                     compardNum(confLoanApplyDO.getLoan_ratio_compare(), loanRate, confLoanApplyDO.getLoan_ratio().divide(new BigDecimal("100")), "贷款比例");
