e1f32e4afc39e5814ac4da9a6f3778823a1d9adc
zhengdu <zhengdu1992@163.com>
2018-10-19 16:47:08 +0800

    v_1.1.12_3 银行开卡清单文件分页问题修复

--- src/main/java/com/yunche/loan/service/impl/BankListFileServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/BankListFileServiceImpl.java
@@ -29,16 +29,26 @@ public class BankListFileServiceImpl implements BankListFileService{
 
     @Override
     public ResultBean detail(Integer pageIndex, Integer pageSize, Long listId, String userName, String idCard, Byte isCustomer) {
-        //查询客户详细信息
+
         PageHelper.startPage(pageIndex, pageSize, true);
-        List<BankFileListRecordDO> bankFileListRecordDOS = bankRecordQueryDOMapper.selectBankRecordDetail(listId,userName,idCard,isCustomer);
-        List<BankFileListRecordVO> collect = bankFileListRecordDOS.stream().map(e -> {
-            BankFileListRecordVO bankFileListRecordVO = new BankFileListRecordVO();
-            BeanUtils.copyProperties(e, bankFileListRecordVO);
-            bankFileListRecordVO.setOrderId(String.valueOf(e.getOrderId()));
-            return bankFileListRecordVO;
-        }).collect(Collectors.toList());
-        PageInfo<BankFileListRecordVO> pageInfo = new PageInfo<>(collect);
+
+        //查询客户详细信息
+        List<BankFileListRecordDO> bankFileListRecordDOS = bankRecordQueryDOMapper.selectBankRecordDetail(listId, userName, idCard, isCustomer);
+
+        PageInfo<BankFileListRecordDO> pageInfo = new PageInfo<>(bankFileListRecordDOS);
+
+        List<BankFileListRecordVO> collect =
+                bankFileListRecordDOS.stream()
+                        .map(e -> {
+
+                            BankFileListRecordVO bankFileListRecordVO = new BankFileListRecordVO();
+                            BeanUtils.copyProperties(e, bankFileListRecordVO);
+                            bankFileListRecordVO.setOrderId(String.valueOf(e.getOrderId()));
+
+                            return bankFileListRecordVO;
+                        })
+                        .collect(Collectors.toList());
+
         return ResultBean.ofSuccess(collect, new Long(pageInfo.getTotal()).intValue(), pageInfo.getPageNum(), pageInfo.getPageSize());
 
     }
