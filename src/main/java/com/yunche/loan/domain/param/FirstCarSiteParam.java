dcc5ed6a570cbcdfb079040f639fb88a473ef040
zhongmingxiao <17757144205@163.com>
2018-11-23 21:59:07 +0800

    fix

--- src/main/java/com/yunche/loan/domain/param/FirstCarSiteParam.java
+++ src/main/java/com/yunche/loan/domain/param/FirstCarSiteParam.java
@@ -1,5 +1,7 @@
 package com.yunche.loan.domain.param;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.Data;
 
 @Data
@@ -10,4 +12,12 @@ public class FirstCarSiteParam
     private String vin;
 
     private String cityName;
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this, new SerializerFeature[]{SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.UseISO8601DateFormat });
+
+    }
 }
