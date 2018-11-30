ab4738a5cb3105474b75d1d44588fcde744ae173
admin <123456>
2018-11-30 10:35:51 +0800

    jtx生产配置

--- src/main/java/com/yunche/loan/config/common/JtxConfig.java
+++ src/main/java/com/yunche/loan/config/common/JtxConfig.java
@@ -1,6 +1,7 @@
 package com.yunche.loan.config.common;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,15 @@ import org.springframework.stereotype.Component;
 @PropertySource(value = "classpath:jtxftpconfig.properties")
 @Data
 public class JtxConfig {
+    @Value("${spring.jtx.jtxServierIP}")
     private String jtxServierIP;
+    @Value("${spring.jtx.jtxUserName}")
     private String jtxUserName;
+    @Value("${spring.jtx.jtxPassword}")
     private String jtxPassword;
+    @Value("${spring.jtx.jtxUrl}")
     private String jtxUrl;
+    @Value("${spring.jtx.jtxPort}")
     private int jtxPort;
     private String jtxTempDirRes;
     private String jtxTempDirSend;
