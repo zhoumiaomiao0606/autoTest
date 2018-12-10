c020b8de33f520066d9efb2f6bd41c347945e09f
zhongmingxiao <17757144205@163.com>
2018-12-10 11:06:57 +0800

    异常处理页面

--- src/main/java/com/yunche/loan/domain/vo/FinancialExceptionOperationVO.java
+++ src/main/java/com/yunche/loan/domain/vo/FinancialExceptionOperationVO.java
@@ -5,4 +5,21 @@ import lombok.Data;
 @Data
 public class FinancialExceptionOperationVO
 {
+    private String orderId;
+
+    private String operationExceptionTime;
+
+    private String customerName;
+
+    private String idCard;
+
+    private String bank;
+
+    private String carType;
+
+    private String partnerName;
+
+    private String operationStatus;
+
+    private String operationExceptionReason;
 }
