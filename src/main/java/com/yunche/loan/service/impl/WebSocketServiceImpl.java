71d5be6fd1768482c81a369b7374b7f9c5f17973
liuzhe <718161567@qq.com>
2019-01-11 22:48:23 +0800

    面签排队  partnerId

--- src/main/java/com/yunche/loan/service/impl/WebSocketServiceImpl.java
+++ src/main/java/com/yunche/loan/service/impl/WebSocketServiceImpl.java
@@ -76,6 +76,7 @@ public class WebSocketServiceImpl implements WebSocketService {
         Preconditions.checkNotNull(webSocketParam.getAnyChatUserId(), "anyChatUserId不能为空");
         if (TYPE_APP.equals(webSocketParam.getType())) {
             Preconditions.checkNotNull(webSocketParam.getOrderId(), "orderId不能为空");
+//            Preconditions.checkNotNull(webSocketParam.getPartnerId(), "partnerId不能为空");
             Preconditions.checkNotNull(webSocketParam.getBankPeriodPrincipal(), "bankPeriodPrincipal不能为空");
         }
 
@@ -206,6 +207,16 @@ public class WebSocketServiceImpl implements WebSocketService {
         if (TYPE_PC.equals(webSocketParam.getType())) {
             return true;
         }
+
+        // partnerId
+        Long partnerId = webSocketParam.getPartnerId();
+        if (null == partnerId) {
+            UniversalInfoVO universalInfoVO = loanQueryDOMapper.selectUniversalInfo(webSocketParam.getOrderId());
+            String partnerId_ = universalInfoVO.getPartner_id();
+            Preconditions.checkArgument(StringUtils.isNotBlank(partnerId_), "当前订单合伙人不存在");
+            partnerId = Long.valueOf(partnerId_);
+        }
+
 
         // 视频面签配置详情
         ConfVideoFaceVO confVideoFaceVO = confVideoFaceService.detail(webSocketParam.getBankId(), webSocketParam.getPartnerId());
