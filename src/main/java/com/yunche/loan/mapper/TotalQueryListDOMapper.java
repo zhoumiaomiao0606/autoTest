8656258e03fcada488e264b3cf937b1ef5cb49d0
admin <123456>
2019-01-16 16:30:39 +0800

    新列表

---
+++ src/main/java/com/yunche/loan/mapper/TotalQueryListDOMapper.java
@@ -0,0 +1,25 @@
+package com.yunche.loan.mapper;
+
+import com.yunche.loan.domain.query.TaskListQuery;
+import com.yunche.loan.domain.vo.TaskListVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface TotalQueryListDOMapper {
+    List<TaskListVO> selectTotalCusInfo(TaskListQuery taskListQuery);
+
+    List<TaskListVO> selectBankCreditPend(TaskListQuery taskListQuery);
+
+    List<TaskListVO> selectBankCreditExport(TaskListQuery taskListQuery);
+
+    List<TaskListVO> selectBankCreditSuccess(TaskListQuery taskListQuery);
+
+    List<TaskListVO> selectBankCreditAll(TaskListQuery taskListQuery);
+
+    List<Long> selectSuccessBankOrder(@Param("transCode")String transCode);
+
+    List<Long> selectProcessBankOrder(@Param("transCode")String transCode);
+}
