ebd3f4ca0a278f13e6d4ff1b86b2650a73664f63
zhengdu <zhengdu1992@163.com>
2019-01-22 19:00:18 +0800

    列表

--- src/main/java/com/yunche/loan/config/feign/client/MultimediauploadClient.java
+++ src/main/java/com/yunche/loan/config/feign/client/MultimediauploadClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "multimediauploadClient", url = "http://172.16.169.66:7003/")
+@FeignClient(name = "multimediauploadClient", url = "http://172.16.169.66:7002/")
 public interface MultimediauploadClient {
 
     @RequestMapping( value = "/api/v1/loanorder/icbc/multimediaupload",method = RequestMethod.POST)
