2230760431e7691af6f8112d53d94a08ba2ba304
zhongmingxiao <17757144205@163.com>
2019-03-26 10:58:26 +0800

    面签--加车辆类型

--- src/main/java/com/yunche/loan/domain/vo/UniversalInfoVO.java
+++ src/main/java/com/yunche/loan/domain/vo/UniversalInfoVO.java
@@ -135,6 +135,8 @@ public class UniversalInfoVO {
     private String bridge_lend_date;
     private String bank_lend_date;
 
+    private String car_type_name;
+
 
     private BigDecimal riskBearRate;
 
